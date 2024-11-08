2f5e16004d2e92bef3c23991fe63a437924b03a4
rajan <rajandhand17@gmail.com>
2024-11-08 09:32:04 -0500

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -26,3 +26,9 @@ function increaseCount() {
   displayCount(); // Display the count
   checkCountValue(); // Check count value and display messages
 }
+
+function resetCount(){
+    count=0;
+    displayCount(); // Display the count
+  checkCountValue(); // Check count value and display messages
+}
